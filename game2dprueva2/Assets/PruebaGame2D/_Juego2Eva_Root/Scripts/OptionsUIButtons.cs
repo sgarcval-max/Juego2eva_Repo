c4486c8ddac4870112258bd01cadbfa77b8d1c47
Santiago-Proyects <sgarcval@gmail.com>
2025-12-10 18:08:28 +0100

    Update menu 3

--- game2dprueva2/Assets/PruebaGame2D/_Juego2Eva_Root/Scripts/OptionsUIButtons.cs
+++ game2dprueva2/Assets/PruebaGame2D/_Juego2Eva_Root/Scripts/OptionsUIButtons.cs
@@ -3,27 +3,26 @@ using UnityEngine.UI;
 
 public class OptionsUIButtons : MonoBehaviour
 {
-    [Header("Opciones UI Buttons")]
-    public Button controlsButton;
+    [Header("Botones de Opciones")]
+    public Button backButton;
     public Button soundButton;
     public Button keyboardButton;
     public Button gamepadButton;
     public Button keyboardReassignButton;
     public Button gamepadReassignButton;
-    public Button backButton; // Volver al Main Menu desde Options
 
-    private void Awake()
+    private void Start()
     {
-        // Comprobamos que existe el singleton
+        // Asegurarse de que MenuControllerRuntime existe
         if (MenuControllerRuntime.Instance == null)
         {
             Debug.LogError("[OptionsUIButtons] No existe MenuControllerRuntime en escena.");
             return;
         }
 
-        // Asignamos los listeners de los botones
-        if (controlsButton != null)
-            controlsButton.onClick.AddListener(() => MenuControllerRuntime.Instance.ShowControlsPanel());
+        // Asignar listeners a los botones
+        if (backButton != null)
+            backButton.onClick.AddListener(() => MenuControllerRuntime.Instance.ReturnToMainMenuFromOptions());
 
         if (soundButton != null)
             soundButton.onClick.AddListener(() => MenuControllerRuntime.Instance.ShowSoundPanel());
@@ -39,8 +38,5 @@ public class OptionsUIButtons : MonoBehaviour
 
         if (gamepadReassignButton != null)
             gamepadReassignButton.onClick.AddListener(() => MenuControllerRuntime.Instance.OpenGamepadReassignPanel());
-
-        if (backButton != null)
-            backButton.onClick.AddListener(() => MenuControllerRuntime.Instance.ReturnToMainMenuFromOptions());
     }
 }

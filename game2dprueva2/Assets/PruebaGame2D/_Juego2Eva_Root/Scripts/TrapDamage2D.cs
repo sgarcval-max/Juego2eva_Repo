3225d57c2347dad59cb9014d4123e430d125e270
AlejandroMartinPerez <almape.alumno@fundacionpjo.es>
2026-02-06 19:41:25 +0100

    programacion trampas

---
+++ game2dprueva2/Assets/PruebaGame2D/_Juego2Eva_Root/Scripts/TrapDamage2D.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class TrapDamage2D : MonoBehaviour
+{
+    public int damage = 1;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(damage);
+            }
+        }
+    }
+}
+
